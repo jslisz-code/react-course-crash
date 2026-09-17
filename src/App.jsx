8c7c8ed4a3d01dec64d290a5b8082fc8f9c5762c
jslisz-code <jslisz.code@outlook.com>
2026-09-17 01:44:21 -0500

    debugging

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Nav from "./components/Nav";
+import Users from "./pages/Users";
+
+
+function App() {
+  return (
+      <Router>
+        <Nav />
+        <Routes>
+           <Route path="/" element={<Home />} />
+           <Route path="/users/:name" element={<Users />} />
+          </Routes>
+        </Router>
+    );
+}
+
+export default App;
